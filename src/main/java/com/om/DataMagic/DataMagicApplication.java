3fef3afab1818d364c0188d74c90d4734869acaa
zhongjun2 <jun.zhongjun2@gmail.com>
2025-01-13 10:28:32 +0800

    Added init project

---
+++ src/main/java/com/om/DataMagic/DataMagicApplication.java
@@ -0,0 +1,13 @@
+package com.om.DataMagic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMagicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMagicApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/om/DataMagic/task/Task.java
@@ -0,0 +1,22 @@
+/*
+ This project is licensed under the Mulan PSL v2.
+ You can use this software according to the terms and conditions of the Mulan PSL v2.
+ You may obtain a copy of Mulan PSL v2 at:
+     http://license.coscl.org.cn/MulanPSL2
+ THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ See the Mulan PSL v2 for more details.
+ Created: 2025
+*/
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TaskManager {
+
+    /**
+     * Logger for logging messages in App class.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(TaskManager.class);
+
+}
