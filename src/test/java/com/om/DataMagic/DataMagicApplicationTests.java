c3596530a962cc4db8e3a5a45046923d6d9d0364
zhongjun2 <jun.zhongjun2@gmail.com>
2025-01-13 10:28:32 +0800

    Added init project

---
+++ src/test/java/com/om/DataMagic/DataMagicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.om.DataMagic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMagicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
